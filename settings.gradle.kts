4b18774ba3e1790a5c3a759a73ce49d21dce27d8
HecMurillo <xjuliopvp@gmail.com>
2023-10-27 12:28:35 -0700

    prueba git

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ds41_practica_2"
+include(":app")
+ 
